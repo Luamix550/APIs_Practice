5dee4315ddbabf49d502230630732e7c4495e53e
Luamix <luizalfonso611@gmail.com>
2024-02-11 15:38:51 -0500

    get api rick and morty

---
+++ index.js
@@ -0,0 +1,25 @@
+function getcharacters(done) {
+
+    const results = fetch("https://rickandmortyapi.com/api/character");
+    results
+        .then(response => response.json())
+        .then(data => {
+            done(data)
+        });
+}
+
+getcharacters(data => {
+
+    data.results.forEach(personaje => {
+        const article = document.createRange().createContextualFragment(/*html*/`
+        <article>
+        <div class="image-container">
+            <img src="${personaje.image}" alt="Personajes">
+        </div>
+            <h2>${personaje.name}</h2>
+            <span>${personaje.status}</span>
+    </article>`)
+    const main = document.querySelector("main")
+    main.append(article)
+    });
+});
